b50e2f0b2cbcec58444629e2ef1df398241a442c
Patryczekk <patryk.sroka@elev.ga.ntig.se>
2020-08-27 20:25:43 +0200

    I like me

---
+++ myFirstServer/app.js
@@ -0,0 +1,30 @@
+console.log("Hej på er")
+
+let name = "Patrick" 
+
+console.log(name)
+
+name += " Stroka" 
+
+console.log(name)
+
+let age = 12
+
+console.log("My age is: " + age)
+
+age ++ 
+
+console.log("My age next year will be " + age)
+
+let alive = false 
+
+if (alive){
+    console.log("You are alive")
+}
+else {
+    console.log("You are dead")
+}
+
+if (name != "Niklas Hjelm"){
+    console.log("Drip too hard")
+}
---
+++ myFirstServer/läxa.js
@@ -0,0 +1,11 @@
+let nrA = 12
+
+let nrB = 15
+
+console.log(nrA += nrB)
+
+console.log(nrA *= nrB)
+
+console.log(nrA -= nrB)
+
+console.log(nrA /= nrB)
